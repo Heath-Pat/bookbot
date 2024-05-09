1d8036e7dd02a19fa9ea5adcdb55182e94c89fca
Heath-Pat <ovouncleheath9850@yahoo.com>
2024-05-09 17:32:21 -0400

    Changed the code so it looks and functions correct

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_content = f.read()
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
+
+
 main()
